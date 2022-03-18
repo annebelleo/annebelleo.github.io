9a9070d40463f5d82798d0b8a22a50155e83c32c
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2022-03-18 19:05:23 +0100

    Updates

---
+++ precache-manifest.846d7c0eb031a0d34159869d1bec48ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "be042ef6578c2763351b",
+    "url": "/static/js/main.9246af27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6b95733b117384b7e6c5",
+    "url": "/static/css/2.44dc4947.chunk.css"
+  },
+  {
+    "revision": "6b95733b117384b7e6c5",
+    "url": "/static/js/2.10d3ad52.chunk.js"
+  },
+  {
+    "revision": "8c9164f313815cda8c1ce4cd7bd909e5",
+    "url": "/static/media/IMG_0002.8c9164f3.JPG"
+  },
+  {
+    "revision": "53d22258f71040ebe9c328290cf45a5c",
+    "url": "/static/media/photo1.53d22258.png"
+  },
+  {
+    "revision": "03181d33984a19a657e0293ef1405915",
+    "url": "/index.html"
+  }
+];
