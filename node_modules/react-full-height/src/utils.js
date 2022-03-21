e3511ea7a92a2994cf0581d082ecb4a5cc40a2c8
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2022-03-21 14:52:08 +0100

    Updates

--- node_modules/react-full-height/src/utils.js
+++
@@ -1,4 +0,0 @@
-export const getPxValue = value => (value || value === 0 ? `${value}px` : "");
-
-export const getResulutionValue = (value, breakpoint) =>
-  value || value === 0 ? getPxValue(value) : getPxValue(breakpoint);
